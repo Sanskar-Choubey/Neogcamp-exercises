aa28363c5a075ed2cb60a01a9851e6c751acee2d
Sanskar Choubey <choubeysanskar03@gmail.com>
2020-12-14 13:43:52 +0530

    Add : textarea and reference it in js

--- Vanilla JS Exercise/1.Wiring button click/apps.js
+++ Vanilla JS Exercise/1.Wiring button click/apps.js
@@ -1,5 +1,7 @@
 var button = document.querySelector(".btn");
+var inputField = document.querySelector(".input-field");
 
 button.addEventListener('click',function clickHandler(){
     console.log("Clicked");
+    console.log("input",inputField.value);
 })
